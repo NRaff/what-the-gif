2793878102f267126c53b944ff05ef2a4de9ae76
darothmedia <87622162+darothmedia@users.noreply.github.com>
2021-12-07 10:40:40 -0800

    auth nav on splash

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -22,8 +22,8 @@ const App = () => (
       <div className='components'>
       <AuthRoute exact path="/login" component={LoginForm} />
       <AuthRoute exact path="/signup" component={SignupForm} />
-      <Route path="/create" component={NewGame} />
-      <Route path="/join" component={JoinGame} />
+      <ProtectedRoute path="/create" component={NewGame} />
+      <ProtectedRoute path="/join" component={JoinGame} />
       </div>
     </Route>
   </div>
--- frontend/src/components/splash/splash.jsx
+++ frontend/src/components/splash/splash.jsx
@@ -5,6 +5,23 @@ import { Link } from "react-router-dom";
 class Splash extends React.Component {
   constructor(props) {
     super(props)
+    this.splashNav = this.splashNav.bind(this)
+  }
+
+  splashNav(){
+    return (
+      (this.props.auth ? (
+        <div id='game-nav'>
+          <Link to='/create'><button>Create Game</button></Link>
+          <Link to='/join'><button>Join Game</button></Link>
+        </div>
+      ) : (
+        <div className='logged-out'>
+          <Link to='/login'>
+            <button id='log-in-button'>Login to Play</button>
+          </Link>
+        </div >
+      )))
   }
 
   render(){
@@ -14,10 +31,11 @@ class Splash extends React.Component {
           <section className='top' />
           <section className='center'>
             <h1>WHAT THE GIF!?</h1>
-            <div id='game-nav'>
+            {this.splashNav()}
+            {/* <div id='game-nav'>
               <Link to='/create'><button>Create Game</button></Link>
               <Link to='/join'><button>Join Game</button></Link>
-            </div>
+            </div> */}
           </section>
           <section className='bottom' />
         </div>
