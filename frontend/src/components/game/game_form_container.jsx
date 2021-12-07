df1fd8f9508c5b6081b8e9a5e1243696243662b7
alexlolas <alexanderlolas@yahoo.com>
2021-12-06 17:32:25 -0800

    fixed some defaults

--- frontend/src/components/game/game_form_container.jsx
+++ frontend/src/components/game/game_form_container.jsx
@@ -5,7 +5,8 @@ import {createGame} from "../../actions/game_actions"
   // State will be set in the form
 const mSTP = state => ({
   // Errors name may be different
-  errors: state.errors.game
+  errors: state.errors.game,
+  currentUser: state.session.user.id
 })
 
 // Need to have functions to createGame, maybe need delete game at some point
