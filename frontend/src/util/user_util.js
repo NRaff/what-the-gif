5226586efaab8d599bff990df56342c607cad1a4
darothmedia <87622162+darothmedia@users.noreply.github.com>
2021-12-07 14:41:15 -0800

    aedit for merge

--- frontend/src/util/user_util.js
+++ frontend/src/util/user_util.js
@@ -1,13 +1,16 @@
 import axios from "axios";
 
 export const setFavoriteGIF = (gif) => (
+  // All routes are TEMP - will need to replace once the backend routes are created
   axios.post('/api/users/fav/create', gif)
 )
 
 export const editFavoriteGIF = (gif) => (
+  // All routes are TEMP - will need to replace once the backend routes are created
   axios.patch('/api/users/fav/edit', gif)
 )
 
 export const removeFavoriteGIF = (gifId) => (
+  // All routes are TEMP - will need to replace once the backend routes are created
   axios.delete('/api/users/fav', gifId)
 )
