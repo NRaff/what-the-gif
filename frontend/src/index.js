bcc1a5a9e05e6c1089714de1a68926ef4a9cf02e
nraff <nraff510@gmail.com>
2021-12-07 10:48:55 -0800

    add giphy_util

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,6 +11,7 @@ import {
   joinGame,
   createGame
 } from './actions/game_actions'
+import { getGameCategories, getGifCategories, searchGifs } from './util/giphy_util';
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;
@@ -19,6 +20,9 @@ document.addEventListener('DOMContentLoaded', () => {
   window.signup = signup
   window.createGame = createGame
   window.joinGame = joinGame
+  window.searchGifs = searchGifs
+  window.getGifCategories = getGifCategories
+  window.getGameCategories = getGameCategories
   
   // If a returning user has a session token stored in localStorage
   if (localStorage.jwtToken) {
---
+++ frontend/src/util/giphy_util.js
@@ -0,0 +1,60 @@
+import { GIPHY_KEY } from "../config";
+import axios from "axios";
+
+const giphy = {
+  search: "https://api.giphy.com/v1/gifs/search",
+  trending: "https://api.giphy.com/v1/gifs/trending",
+  categories:"https://api.giphy.com/v1/gifs/categories"
+}
+
+// always set the api key as 
+// the first param after the endpoint
+let params = {
+  api_key: `?api_key=${GIPHY_KEY}`,
+  q: "&q=",
+  limit: "&limit=",
+  offset: "&offset=",
+  rating: "&rating=pg-13",
+  lang: "lang=en",
+}
+
+const greenCategories = [
+  "actions",
+  "memes",
+  "reactions"
+]
+
+export const searchGifs = searchTerm => {
+  const requestItems = [
+    giphy.search,
+    params.api_key,
+    params.rating,
+    params.q,
+    searchTerm,
+  ]
+  const requestUrl = requestItems.join('')
+  return (
+    axios.get(requestUrl)
+  )
+}
+
+export const getGifCategories = () => (
+  axios.get(giphy.categories + params.api_key)
+)
+
+// does not hit an api
+export const getGameCategories = payload => {
+  // debugger
+  const categories = payload.data.data.filter(sub => (
+    greenCategories.includes(sub.name)
+  ))
+  let gameCategories = []
+  categories.forEach(cat => (
+    gameCategories = gameCategories.concat(cat.subcategories)
+  ))
+  return gameCategories
+}
+
+
+
+
