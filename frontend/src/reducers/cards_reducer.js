6dac888ebb6c10127e1b5ce66ba45b4b349962ea
alexlolas <alexanderlolas@yahoo.com>
2021-12-07 14:44:21 -0800

    add some stuff

--- frontend/src/reducers/cards_reducer.js
+++ frontend/src/reducers/cards_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import gameDeck from "./game_deck_reducer";
+import playedCardsReducer from "./played_cards_reducer";
 
 const cards = combineReducers({
-  gameDeck: gameDeck
+  gameDeck: gameDeck,
+  playedCards: playedCardsReducer
 })
 
 export default cards
