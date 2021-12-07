18d754540284fbd9be05122ed023f9fabfa2eeae
darothmedia <87622162+darothmedia@users.noreply.github.com>
2021-12-07 12:23:02 -0800

    user profile setup

---
+++ frontend/src/components/profile/profile.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from 'react-router-dom'
+import '../../stylesheets/root.scss'
+
+class Profile extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render(){
+    return(
+      <div></div>
+    )
+  }
+}
+
+export default Profile
