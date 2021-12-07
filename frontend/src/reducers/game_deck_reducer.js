b58475a531a05853a2a3e4feb694d72eebc02f95
alexlolas <alexanderlolas@yahoo.com>
2021-12-07 11:02:38 -0800

    All cards made

--- frontend/src/reducers/game_deck_reducer.js
+++ frontend/src/reducers/game_deck_reducer.js
@@ -2,13 +2,14 @@ import { RECEIVE_ALL_CARDS } from "../actions/game_deck_actions";
 
 const gameDeck = (state = {}, action) => {
   Object.freeze(state)
-
+  const nextState = Object.assign({}, state)
   switch (action.type) {
     case RECEIVE_ALL_CARDS:
       let allCards = Object.values(action.cards)
       allCards.map((card, idx) => {
-        
+        nextState[idx + 1] = card
       }) 
+      return nextState
 
     default:
       return state;
