ccfa2d70765bb1a2c9a9f7083ac89dbd3a88fe69
mattpettenato <mattpettenato@gmail.com>
2021-12-07 16:49:39 -0800

    fixed

--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -2,21 +2,13 @@ import { combineReducers } from "redux";
 import UserReducer from "./user_reducer";
 import GameReducer from "./game_reducer";
 import cards from "./cards_reducer";
-<<<<<<< HEAD
-import CategoriesReducer from "./categories_reducer"
-=======
 import SearchedGifsReducer from "./searched_gifs_reducer"
->>>>>>> f7d22def4469c34c48bf9f842c02df2f434ce8d3
 
 const EntitiesReducer = combineReducers({
   users: UserReducer,
   games: GameReducer,
   cards: cards,
-<<<<<<< HEAD
-  categories: CategoriesReducer
-=======
   searchgifs: SearchedGifsReducer
->>>>>>> f7d22def4469c34c48bf9f842c02df2f434ce8d3
 })
 
 export default EntitiesReducer;
