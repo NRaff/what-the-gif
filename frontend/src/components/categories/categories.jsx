182ebf55a3d23069d9ff7e2277ecbf0de635f18e
alexlolas <alexanderlolas@yahoo.com>
2021-12-08 12:00:34 -0800

    category skeltons

---
+++ frontend/src/components/categories/categories.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Categories extends React.Component{
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default Categories
---
+++ frontend/src/components/categories/categories_container.jsx
@@ -0,0 +1,13 @@
+import { connect } from "react-redux";
+import Categories from "./categories";
+
+const mSTP = state => ({
+
+})
+
+
+const mDTP = dispatch => ({
+
+})
+
+export default connect(mSTP, mDTP)(Categories)
