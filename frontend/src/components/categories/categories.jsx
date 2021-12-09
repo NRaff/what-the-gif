a595d835fa12f952a7ee35c0dfa35a2c22cf3720
alexlolas <alexanderlolas@yahoo.com>
2021-12-08 17:00:40 -0800

    fix syntax

--- frontend/src/components/categories/categories.jsx
+++ frontend/src/components/categories/categories.jsx
@@ -6,39 +6,29 @@ class Categories extends React.Component{
   constructor(props){
     super(props)
     this.handleSubmit = this.handleSubmit.bind(this)
-  
   }
 
   componentDidMount(){
     this.props.fetchCategories()
-
   }
 
   handleSubmit(randomCategory){
-    // e.preventDefault()
-
     this.props.playCategory(randomCategory[0])
-    this.setState({
-      category: ''
-    })
   }
 
   render(){
  
   if (!this.props.deckCategories.length > 0) return null
-  let length = this.props.deckCategories.length - 1
+
+   let length = this.props.deckCategories.length - 1
    let randomIndex = randomLength(0, length)
    let randomCategory = [this.props.deckCategories[randomIndex]]
-  //  this.setState(randomCategory)
-  //  console.log(this.state)
- 
     return (
       <div>
         <div id="categories">
           {randomCategory.map(category => (          
             category.name   
-          ))}
-          
+          ))}      
         </div>
         <button onClick={() => this.handleSubmit(randomCategory)}></button>
       </div>
