46cb65344ec2fe3101b302d277a595b2b94dee39
nraff <nraff510@gmail.com>
2021-12-08 20:21:17 -0800

    clean main before branching

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,6 @@ import configureStore from './store/store';
 import jwt_decode from 'jwt-decode';
 import * as SessionAPI from './util/session_api_util';
 import { logout } from './actions/session_actions';
-import { logout } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;
