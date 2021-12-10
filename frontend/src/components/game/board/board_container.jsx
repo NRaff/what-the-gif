381797c8f38c9d59399f30503307802ec2e3d9e8
nraff <nraff510@gmail.com>
2021-12-09 17:48:44 -0800

    fix board container

--- frontend/src/components/game/board/board_container.jsx
+++ frontend/src/components/game/board/board_container.jsx
@@ -15,7 +15,7 @@ const mSTP = (state, ownProps) => {
     players: Object.values(state.entities.users),
     currentUser: state.session.user,
     users: Object.values(state.entities.users),
-    categories: Object.values(state.entities.categories.deckCategory)
+    categories: Object.values(state.entities.categories.deckCategory),
     over: state.ui.roundOver,
     submittedCards: state.entities.cards.submittedCards
   })
