916b3c640d9f15de19d19b2f46e57fd0b42a14ee
nraff <nraff510@gmail.com>
2021-12-21 08:36:35 -0800

    fix giphy key

--- frontend/src/util/giphy_util.js
+++ frontend/src/util/giphy_util.js
@@ -1,5 +1,6 @@
-import { GIPHY_KEY } from "../config";
-import { PROD_GIPHY_KEY } from "../config";
+// import { GIPHY_KEY } from "../config";
+import GIPHY_KEY from "../config"
+// import { PROD_GIPHY_KEY } from "../config";
 import axios from "axios";
 
 const giphy = {
